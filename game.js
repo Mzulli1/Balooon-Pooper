2ee62a26e8474b022185f596835bf3443fb6ece1
Mzulli1 <mzulli1@binghamton.edu>
2020-04-17 19:10:54 -0400

    Update 2
    Started implementation of three.js
      Goals:
    
    Make a sphere that can grow on event
    Make the canvas resize automatically

--- game.js
+++ game.js
@@ -1,9 +1,30 @@
-console.log("hello");
+function start(){
+ 
+ $("#startpage").hide();
+ $("canvas").show();
+
+
+
+ let popped = true;
+
+ let players = $("#spinner").value();
+
+ while (!popped){
+
+
+
+ }
+
+ 
+
+}
 
 $(document).ready(function(){
   
+  	$("canvas").hide();
+	$("#start").click(start);
 
 
   
-  
+
 });
